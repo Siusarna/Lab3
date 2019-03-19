ee8d7aa1d40f0630533b626bdae82a32cf705bed
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-03-19 21:01:50 +0200

    upper_case fixed

--- Project1/Project1/Wild_Nixon_Functions.cpp
+++ Project1/Project1/Wild_Nixon_Functions.cpp
@@ -13,6 +13,7 @@ void work_with_string(string &str, Linked_List *arr, int n) {
 		cword = str.substr(0, (str.find_first_of(" ")));
 		upper_case(cword);
 		hash = hash_function(cword, n);
+		cout << cword << endl;
 		(arr[hash]).search(cword);
 		str = str.substr(str.find_first_of(" ") + 1);
 	}
@@ -21,9 +22,12 @@ void work_with_string(string &str, Linked_List *arr, int n) {
 void upper_case(string &str) {
 	char help;
 	for (int i = 0; i < str.size(); i++) {
-		if ((str[i] > 'a') && (str[i] < 'z')) {
+		if ((str[i] >= 'a') && (str[i] <= 'z')) {
 			help = str[i];
 			help = 65 + (help - 97);
+			cout << help;
+			str[i] = help;
+			cout << str[i];
 		}
 	}
 }
